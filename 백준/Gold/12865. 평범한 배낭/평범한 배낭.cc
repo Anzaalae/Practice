60ed527d55ca3a4bc7011ca597e5b74fc7042367
Anzaalae <81411273+Anzaalae@users.noreply.github.com>
2024-09-08 19:52:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -5,36 +5,52 @@ int main() {
     int n, pack;
     cin>> n>> pack;
 
-    //int memoiz[pack + 1] = {};
-    int memoiz[n][pack + 1] = {};
+    int memoiz[pack + 1] = {};
 
     int weight;
     int value;
     for(int i= 0; i< n; i++) {
         cin>> weight>> value;
         
+        // switch(i) {
+        //     case 0:
+        //         for(int j = weight; j <= pack; j++) {
+        //             memoiz[0][j] = value;
+        //         }
+        //         break;
+        //     default:
+        //         for(int j = 1; j <= pack; j++) {
+        //             if(j < weight) {
+        //                 memoiz[i][j] = memoiz[i-1][j];
+        //             } else {
+        //                 if(memoiz[i-1][j] < memoiz[i-1][j-weight] + value) {
+        //                     memoiz[i][j] = memoiz[i-1][j-weight] + value;
+        //                 } else {
+        //                     memoiz[i][j] = memoiz[i-1][j];
+        //                 }
+        //             }
+        //         }
+        // }
+
+        // 하나 입력 받을 때 마다 배열의 index가 무게, 값이 가치를 의미하는 dp 테이블 업데이트
+        // 역순으로 확인해보자!
+
         switch(i) {
             case 0:
-                for(int j = weight; j <= pack; j++) {
-                    memoiz[0][j] = value;
+                for(int i= weight; i<= pack; i++) {
+                    memoiz[i] = value;
                 }
                 break;
             default:
-                for(int j = 1; j <= pack; j++) {
-                    if(j < weight) {
-                        memoiz[i][j] = memoiz[i-1][j];
-                    } else {
-                        if(memoiz[i-1][j] < memoiz[i-1][j-weight] + value) {
-                            memoiz[i][j] = memoiz[i-1][j-weight] + value;
-                        } else {
-                            memoiz[i][j] = memoiz[i-1][j];
+                for(int i = pack; i >= weight; i--) {
+                    if(i >= weight) {
+                        if(memoiz[i] < memoiz[i-weight] + value) {
+                            memoiz[i] = memoiz[i-weight] + value;
                         }
                     }
                 }
         }
 
-        // 하나 입력 받을 때 마다 배열의 index가 무게, 값이 가치를 의미하는 dp 테이블 업데이트
-        // 역순으로 확인해보자!
         // for(int i= weight; i<= pack; i++) {
         //     if(memoiz[i] < memoiz[i-weight] + value) {
         //         memoiz[i] = memoiz[i-weight] + value;
@@ -52,9 +68,9 @@ int main() {
         // }
 
         // for(int k= 0; k<= pack; k++) {
-        //     cout<< memoiz[i][k]<< ' ';
+        //     cout<< memoiz[k]<< ' ';
         // } cout<< '\n';
     }
 
-    cout<< memoiz[n-1][pack];
+    cout<< memoiz[pack];
 }
